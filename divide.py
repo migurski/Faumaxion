76ef3b5b88d03cf73b92b7bbe4582c667d9ffc53
migurski <migurski@21872b64-488e-dc11-991f-0002b3daa52e>
2007-11-21 00:58:10 +0000

    git-svn-id: svn+ssh://or.pair.com/usr/home/migurski/var/svn/icosagnomon@24 21872b64-488e-dc11-991f-0002b3daa52e

--- divide.py
+++ divide.py
@@ -1,123 +1,138 @@
-import sys, math, icosahedron, transform
+import os, os.path, sys, math, icosahedron, transform, struct
 import PIL.Image as Image
 from PIL.ImageDraw import ImageDraw
 
 UP = 'triangle points up'
 DOWN = 'triangle points down'
 
-def decompose(face, planar_points, point, depth=0):
+world = open('world.topo.bathy.200407.v', 'r')
+
+def latlon_rgba(lat, lon):
+    """ Get (r, g, b) color at specified (lat, lon) in degrees.
+    """
+    w, h = 86400, 43200
+    
+    x, y = (lon + 180) * 240, (90 - lat) * 240
+    x, y = int(x) % w, int(y) % h
+    
+    off = (y * w) + x
+    
+    world.seek(0x40 + off * 3)
+    r, g, b = struct.unpack('BBB', world.read(3))
+    
+    return r, g, b, 0xFF
+
+def decompose(face, name, planar_points, point, depth=4):
     """ Vertices are expected in clockwise order, with first being the point.
     """
     if point == UP:
         pass
-    
     elif point == DOWN:
         pass
-    
     else:
         raise Exception('Unknown point')
     
+    print 'working on', name
+    
+    dim = 256
+    
+    # image x, y coordinates of triangle points
+    if point == UP:
+        coords = (dim/2, dim + dim * math.sin(-math.pi/3)), (dim, dim), (0, dim)
+    elif point == DOWN:
+        coords = (dim/2, dim * math.sin(math.pi/3)), (0, 0), (dim, 0)
+
+    (px1, py1), (px2, py2), (px3, py3) = planar_points
+    (cx1, cy1), (cx2, cy2), (cx3, cy3) = coords
+    
+    # the transform from planar points to image x, y coords
+    t = transform.deriveTransformation(px1, py1, cx1, cy1, px2, py2, cx2, cy2, px3, py3, cx3, cy3)
+    
     if depth:
-        (px1, py1), (px2, py2), (px3, py3) = planar_points
-        
+        # there's further to go; gather up sub-triangles and stitch them together
         px12, py12 = .5 * (px1 + px2), .5 * (py1 + py2)
         px23, py23 = .5 * (px2 + px3), .5 * (py2 + py3)
         px31, py31 = .5 * (px3 + px1), .5 * (py3 + py1)
         
-        # i, l, j, k... (or w, d, a, s)
-        decompose(face, ((px1, py1), (px12, py12), (px31, py31)), point, depth - 1)
-        decompose(face, ((px12, py12), (px2, py2), (px23, py23)), point, depth - 1)
-        decompose(face, ((px31, py31), (px23, py23), (px3, py3)), point, depth - 1)
-        decompose(face, ((px23, py23), (px31, py31), (px12, py12)), {UP:DOWN, DOWN:UP}[point], depth - 1)
+        parts = [('i', ((px1, py1), (px12, py12), (px31, py31))),
+                 ('l', ((px12, py12), (px2, py2), (px23, py23))),
+                 ('j', ((px31, py31), (px23, py23), (px3, py3))),
+                 ('k', ((px23, py23), (px31, py31), (px12, py12)))]
+        
+        parts.sort()
+        
+        surtile = Image.new('RGBA', (dim*2, dim*2), (0x00, 0x00, 0x00, 0x00))
+        
+        # i, l, j, k...
+        for p, coords in parts:
+            if p == 'k':
+                subtile = decompose(face, name + '/' + p, coords, {UP:DOWN, DOWN:UP}[point], depth - 1)
+            else:
+                subtile = decompose(face, name + '/' + p, coords, point, depth - 1)
+                
+            if point == UP:
+                if p == 'i':
+                    x, y = dim/2, int(dim + dim * math.sin(-math.pi/3))
+                elif p == 'l':
+                    x, y = dim, dim
+                elif p == 'j':
+                    x, y = 0, dim
+                elif p == 'k':
+                    x, y = dim/2, int(2*dim + dim * math.sin(-math.pi/3))
+            elif point == DOWN:
+                if p == 'i':
+                    x, y = dim/2, int(-dim * math.sin(-math.pi/3))
+                elif p == 'l':
+                    x, y = 0, 0
+                elif p == 'j':
+                    x, y = dim, 0
+                elif p == 'k':
+                    x, y = dim/2, int(-dim - dim * math.sin(-math.pi/3))
 
+            surtile.paste(subtile, (x, y), subtile)
+
+        colors, mask = surtile.resize((dim, dim), Image.BICUBIC), surtile.resize((dim, dim), Image.LINEAR)
+
+        tile = Image.new('RGBA', (dim, dim), (0x00, 0x00, 0x00, 0x00))
+        tile.paste(colors, (0, 0), mask)
+
+        for y in range(dim):
+            for x in range(dim):
+                alpha = tile.getpixel((x, y))[3]
+                if 0x00 < alpha and alpha < 0xFF:
+                    lat, lon = face.unproject_point(*t.unapply(x, y))
+                    tile.putpixel((x, y), latlon_rgba(lat, lon))
+        
     else:
-        # deriveTransformation(a1x, a1y, a2x, a2y, b1x, b1y, b2x, b2y, c1x, c1y, c2x, c2y)
-        
-        dim = 256
-        mul = 1
-        
-        tile = Image.new('RGB', (dim*mul, dim*mul), (0x00, 0x00, 0x00))
-        draw = ImageDraw(tile)
-        
-        if point == UP:
-            coords = (dim * math.cos(-math.pi/3), dim + dim * math.sin(-math.pi/3)), (dim, dim), (0, dim)
-        elif point == DOWN:
-            coords = (dim * math.cos(math.pi/3), dim * math.sin(math.pi/3)), (0, 0), (dim, 0)
-            
-        draw.polygon([(x*mul, y*mul) for (x, y) in coords], fill=(0xFF, 0xFF, 0xFF))
+        # this is it, go pixel-by-pixel
+        tile = Image.new('RGBA', (dim, dim), (0x00, 0x00, 0x00, 0x00))
+        mask = Image.new('L', (dim, dim), 0x00)
 
-        if mul > 1:
-            tile = tile.resize((dim, dim), Image.ANTIALIAS)
+        ImageDraw(mask).polygon(coords, fill=0xFF)
+
+        for y in range(dim):
+            for x in range(dim):
+                if mask.getpixel((x, y)) != 0x00:
+                    lat, lon = face.unproject_point(*t.unapply(x, y))
+                    tile.putpixel((x, y), latlon_rgba(lat, lon))
+    
+    filename = name + '.png'
+    print 'saving', filename
+    
+    try:
+        tile.save(filename)
+    except IOError:
+        os.makedirs(os.path.dirname(filename))
+        tile.save(filename)
+
+    return tile
+
+if __name__ == '__main__':
+    
+    for f in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20):
+        face = icosahedron.faces[f]
+        latlons = map(icosahedron.vertex2latlon, face.vertices())
+        xys = map(face.project_vertex, face.vertices())
         
-        (px1, py1), (px2, py2), (px3, py3) = planar_points
-        (cx1, cy1), (cx2, cy2), (cx3, cy3) = coords
-        
-        # the transform from planar points to image coords
-        t = transform.deriveTransformation(px1, py1, cx1, cy1, px2, py2, cx2, cy2, px3, py3, cx3, cy3)
-        
-        input = Image.open('world.1440x720.jpg')
-        
-        for tile_y in range(dim):
-            print tile_y, '...'
-        
-            for tile_x in range(dim):
-                if tile.getpixel((tile_x, tile_y)) != (0x00, 0x00, 0x00):
-                    lat, lon = face.unproject_point(*t.unapply(tile_x, tile_y))
-
-                    input_x, input_y = (lon + 180) * 4, (90 - lat) * 4
-                    input_x, input_y = int(input_x) % input.size[0], int(input_y) % input.size[1]
-                    
-                    # print (tile_x, tile_y), (lat, lon), (input_x, input_y)
-                    
-                    tile.putpixel((tile_x, tile_y), input.getpixel((input_x, input_y)))
-        
-        tile.save('tile.png')
-        
-        geo_locations = [face.unproject_point(x, y) for (x, y) in planar_points]
-        
-        print planar_points
-        print geo_locations
-        draw.polygon([(lon * mul, (90 - lat) * mul) for (lat, lon) in geo_locations], outline=0xFF)
-        draw.polygon([((lon - 360) * mul, (90 - lat) * mul) for (lat, lon) in geo_locations], outline=0x99)
-        draw.polygon([((lon + 360) * mul, (90 - lat) * mul) for (lat, lon) in geo_locations], outline=0x99)
-
-mul = 4
-
-# dim = 256
-# mul = 4
-# 
-# tile = Image.new('L', (dim*mul, dim*mul), 0x00)
-# draw = ImageDraw(tile)
-# 
-# #coords = 0, 0, dim*mul, 0, dim*mul * math.cos(math.pi/3), dim*mul * math.sin(math.pi/3)
-# #draw.polygon(coords, fill=0x80)
-# #print coords
-# 
-# coords = (0, dim*mul), (dim*mul, dim*mul), (dim*mul * math.cos(-math.pi/3), dim*mul + dim*mul * math.sin(-math.pi/3))
-# decompose(coords)
-# 
-# tile = tile.resize((dim, dim), Image.ANTIALIAS)
-# tile.save('tile.png')
-
-world = Image.new('L', (360 * mul, 180 * mul), 0x00)
-draw = ImageDraw(world)
-
-for lat in (0, 90):
-    for lon in (0, 90, 180, 270):
-        draw.rectangle((lon*mul, lat*mul, (lon + 90) * mul, (lat + 90) * mul), outline=0x66)
-
-face = icosahedron.faces[20]
-latlons = map(icosahedron.vertex2latlon, face.vertices())
-xys = map(face.project_vertex, face.vertices())
-
-(x1, y1), (x2, y2), (x3, y3) = xys
-# coords = (0, dim), (dim, dim), (dim * math.cos(-math.pi/3), dim + dim * math.sin(-math.pi/3))
-
-#print latlons
-#print xys
-#print coords
-
-decompose(face, xys, UP)
-
-world = world.resize((720, 360), Image.ANTIALIAS)
-world.save('world.png')
+        (x1, y1), (x2, y2), (x3, y3) = xys
+        decompose(face, 'out/tiles/%02d' % f, xys, UP)
